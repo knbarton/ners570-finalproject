5db14c2acc07b764edd24bde9095dabb643a74a0
djwatkins <darryljwatkins@gmail.com>
2020-11-20 20:29:57 -0500

    add initial test script - isn't complete

---
+++ tests/test_mod_comp_next_step.py
@@ -0,0 +1,6 @@
+"""This script is for testing the mod_comp_next_step.f90 subroutines"""
+import numpy as np
+import mod_comp_next_step # won't showup until f2py is run on the mod_comp_next_step
+
+def test_compute_next_u():
+   print("test failing") 
