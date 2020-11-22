25fa334e41a81affe46f7d26ec4e076e4cc0d68d
djwatkins <darryljwatkins@gmail.com>
2020-11-21 21:59:08 -0500

    removed the attempted test script. That should be done in a separate branch

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python -m numpy.f2py -c src/model/mod_comp_next_step.f90 -m src/model/mod_comp_next_step
-
-pytest -vv tests/test_mod_comp_next_step.py
--- tests/test_mod_comp_next_step.py
+++
@@ -1,9 +0,0 @@
-"""This script is for testing the mod_comp_next_step.f90 subroutines"""
-import numpy as np
-from src.model import mod_comp_next_step as mcns # won't showup until f2py is run on the mod_comp_next_step
-
-def test_compute_next_u():
-   next_u = mcns.compute_next_u(201,201,np.zeros((201,201)),np.zeros((201,201)),9.8,
-                               np.zeros((201,201)),1.0,1.0,0.02)
-   assert np.shape(next_u) == (201,201)
-   assert next_u.all() == np.zeros((201,201)).all() 
