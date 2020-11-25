a7e8ffb0ff877656279be141a769e1d3c503e517
djwatkins <darryljwatkins@gmail.com>
2020-11-25 17:18:04 -0600

    add basic Cmake file and build directory

---
+++ build/CMakeFiles/prog.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/src/model/mod_centered_diff.f90" "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/build/CMakeFiles/prog.dir/src/model/mod_centered_diff.f90.o"
+  "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/src/model/mod_comp_next_step.f90" "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/build/CMakeFiles/prog.dir/src/model/mod_comp_next_step.f90.o"
+  "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/src/model/mod_initialize.f90" "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/build/CMakeFiles/prog.dir/src/model/mod_initialize.f90.o"
+  "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/src/model/tsunami.f90" "/home/djwatkins/projects/michigan/github/forks/ners570-finalproject/build/CMakeFiles/prog.dir/src/model/tsunami.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
