9eef7cd2de58cbc99d9df32e7e23908cd3b56288
djwatkins <darryljwatkins@gmail.com>
2020-12-08 20:46:13 -0500

    add python parallelized code

---
+++ src/processing/plot_tsunami_parallel.py
@@ -0,0 +1,59 @@
+"""A parallelized version of plot_tsunami.py"""
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+import imageio
+
+import multiprocessing as mp
+
+import time # https://stackoverflow.com/questions/7370801/how-to-measure-elapsed-time-in-python
+
+def plot_png(n):
+    """This function plots the individual results from the output file"""
+    name = str(n)
+    file_path = path+name+'.out'
+    if os.path.exists(file_path):
+        data = pd.read_csv(file_path,sep='\s+',header=None)
+        data = pd.DataFrame(data)
+        fig = plt.figure()
+        ax = plt.imshow(data.T, vmin=0, vmax=0.5, cmap='jet')
+        plt.gca().invert_yaxis()
+        plt.jet()
+        plt.colorbar()
+        plt.title(str(name))
+        plt.savefig('output/'+name+'.png')
+        plt.close()
+
+
+path = 'output/'
+images = []
+
+start_parallel_time = time.time()
+if __name__ == '__main__':
+    with mp.Pool(8) as p:
+        p.map(plot_png, range(0,10001,10))
+end_parallel_time = time.time()
+parallel_time = end_parallel_time - start_parallel_time
+
+start_loop_time = time.time()
+for n in range(0,10001,10):
+    name = str(n)
+    file_path = path+name+'.out'
+    if os.path.exists(file_path):
+        #plot_png(name)
+        images.append(imageio.imread(path+name+'.png'))
+end_loop_time = time.time()
+loop_time = end_loop_time - start_loop_time
+
+start_imageio_time = time.time()
+imageio.mimsave(path+'tsunami.gif', images)
+end_imageio_time = time.time()
+imageio_time = end_imageio_time - start_imageio_time
+
+print(f"The parallel time is {parallel_time} seconds")
+print(f"The loop time is {loop_time} seconds")
+print(f"The imageio time is {imageio_time} seconds")
+print(f"Total time is {parallel_time + loop_time + imageio_time} seconds")
